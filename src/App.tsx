7b1ca9d2448f43289b864be8c82bd19ac33b2c4d
Nam Seob Seo <nsclass@gmail.com>
2021-04-24 22:45:47 -0500

    upgrade forge and electron

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,35 @@ import ReactDOM from "react-dom";
 import { css } from "@emotion/core";
 
 const App = () => {
-    return (
-        <section className="section">
-            <div className="container">
-                <h1 className="title">
-                    <span
-                        css={css`
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          <span
+            css={css`
                             color: red;
                         `}
-                    >
-                        Hello
+          >
+            Hello
                     </span>{" "}
                     World
                 </h1>
-                <p className="subtitle">
-                    Nam's electron application{" "}
-                    <strong>
-                        <span
-                            css={css`
+        <p className="subtitle">
+          Nam's electron application{" "}
+          <strong>
+            <span
+              css={css`
                                 color: blue;
                             `}
-                        >
-                            template
+            >
+              template
                         </span>
-                    </strong>
+          </strong>
                     !
                 </p>
-            </div>
-        </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
